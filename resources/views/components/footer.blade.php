311fbba8c2bac857dda55a6f03c11a843067f347
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-14 13:30:22 +0545

    add-footer-coomponents

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,57 +1,51 @@
- <section class="footer-section">
-     <link rel="stylesheet" href="{{ asset('css/components/footer.css') }}">
-     <div class="container">
-         <div class="row g-4">
-             <div class="col-lg-4">
-                 <img src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg" alt="Logo">
-                 <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
-                 <div class="d-flex gap-2 mt-3 social-icons">
-                     <a href="#" title=""><i class="bi bi-twitter"></i></a>
-                     <a href="#" title=""><i class="bi bi-facebook"></i></a>
-                     <a href="#" title=""><i class="bi bi-instagram"></i></a>
-                     <a href="#" title=""><i class="bi bi-github"></i></a>
-                 </div>
-             </div>
+<section class="footer-section">
+    <link rel="stylesheet" href="{{ asset('css/components/footer.css') }}">
+    <div class="container">
+        <div class="row g-4">
+            <div class="col-lg-4">
+                <img src="{{asset('logo.png')}}" alt="Logo" class="footer-logo">
+                <p>WorkBridge connects talented freelancers with businesses looking for skilled professionals. Our platform offers opportunities to work on exciting projects that match your skills.</p>
+            </div>
 
-             <div class="col-lg-2">
-                 <h6 class="footer-heading">Company</h6>
-                 <ul class="footer-links list-unstyled mt-3">
-                     <li><a href="#">About</a></li>
-                     <li><a href="#">Features</a></li>
-                     <li><a href="#">Works</a></li>
-                     <li><a href="#">Career</a></li>
-                 </ul>
-             </div>
+            <div class="col-lg-2">
+                <h6 class="footer-heading">Company</h6>
+                <ul class="footer-links list-unstyled mt-3">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">How It Works</a></li>
+                    <li><a href="#">Our Team</a></li>
+                    <li><a href="#">Careers</a></li>
+                </ul>
+            </div>
 
-             <div class="col-lg-2">
-                 <h6 class="footer-heading">Help</h6>
-                 <ul class="footer-links list-unstyled mt-3">
-                     <li><a href="#">Customer Support</a></li>
-                     <li><a href="#">Terms & Conditions</a></li>
-                     <li><a href="#">Privacy Policy</a></li>
-                     <li><a href="#">Sitemap</a></li>
-                 </ul>
-             </div>
+            <div class="col-lg-2">
+                <h6 class="footer-heading">Help</h6>
+                <ul class="footer-links list-unstyled mt-3">
+                    <li><a href="#">Customer Support</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                </ul>
+            </div>
 
-             <div class="col-lg-2">
-                 <h6 class="footer-heading">Resources</h6>
-                 <ul class="footer-links list-unstyled mt-3">
-                     <li><a href="#">Free Ebooks</a></li>
-                     <li><a href="#">Development Tutorial</a></li>
-                     <li><a href="#">How to Blog</a></li>
-                     <li><a href="#">Case Studies</a></li>
-                 </ul>
-             </div>
+            <div class="col-lg-2">
+                <h6 class="footer-heading">Resources</h6>
+                <ul class="footer-links list-unstyled mt-3">
+                    <li><a href="#">Freelancing Tips</a></li>
+                    <li><a href="#">Freelance Guide</a></li>
+                    <li><a href="#">Project Management Tools</a></li>
+                    <li><a href="#">Case Studies</a></li>
+                </ul>
+            </div>
 
-             <div class="col-lg-2">
-                 <h6 class="footer-heading">Extras</h6>
-                 <ul class="footer-links list-unstyled mt-3">
-                     <li><a href="#">Newsletter</a></li>
-                     <li><a href="#">FAQs</a></li>
-                     <li><a href="#">Jobs</a></li>
-                     <li><a href="#">Support</a></li>
-                 </ul>
-             </div>
-         </div>
-     </div>
- </section>
+            <div class="col-lg-2">
+                <h6 class="footer-heading">Extras</h6>
+                <ul class="footer-links list-unstyled mt-3">
+                    <li><a href="#">Newsletter</a></li>
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Job Board</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
