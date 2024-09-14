45fef16a80b3d73e4afda0368e91763058112628
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-14 13:51:07 +0545

    created-landing-section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,30 +14,24 @@
         <section class="pt-12 pb-12 pb-sm-16 pt-lg-8">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-6 text-center text-lg-start">
+                    <div class="col-lg-6 text-center text-lg-start ">
                         <h1 class="display-4 font-weight-bold text-dark">Kickstart Your Freelancing Career Today!</h1>
-                        <p class="mt-3 lead text-muted">Join thousands of developers and designers who have already started their freelancing journey on WorkBridge. Whether you're looking for short-term gigs or long-term projects, we connect you with clients worldwide and help you build a career on your own terms.</p>
-                        <div class="d-flex justify-content-center justify-content-lg-start mt-4">
-                            <div class="d-flex align-items-center me-4">
-                                <p class="h4 font-weight-medium text-dark">2943</p>
-                                <p class="ms-2 text-muted">Freelancers<br>Enrolled</p>
-                            </div>
-
-                            <div class="d-flex align-items-center">
-                                <p class="h4 font-weight-medium text-dark">$1M+</p>
-                                <p class="ms-2 text-muted">Projects<br>Completed</p>
-                            </div>
+                        <p class="mt-3 lead text-muted">
+                            Join thousands of developers and designers who have already started their freelancing journey on WorkBridge. Whether you're looking for short-term gigs or long-term projects, we connect you with clients worldwide and help you build a career on your own terms.
+                        </p>
+                        <div class="text-left">
+                            <a href="#" class="btn btn-primary btn-lg active " role="button" aria-pressed="true">Get Started</a>
                         </div>
                     </div>
-
                     <div class="col-lg-6">
-                        <img class="img-fluid" src="https://d33wubrfki0l68.cloudfront.net/d6f1462500f7670e0db6b76b35054a081679a5a0/0ce15/images/hero/5.1/illustration.png" alt="" />
+                        <img class="img-fluid" src="{{asset('hero.png')}}" alt="hero" />
                     </div>
                 </div>
             </div>
         </section>
     </div>
-    
+
+
     <!-- footer components -->
     @include('components.footer');
 </body>
