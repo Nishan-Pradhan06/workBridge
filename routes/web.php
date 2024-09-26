274048e40031ead48d8dd344204241b9ad60efda
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-26 21:14:44 +0545

    create-> model,controller,table,and set path route

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\JobPostController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index']);
 Route::get('/login',[PageController::class,'login']);
 Route::get('/signup',[PageController::class,'signup']);
+
+Route::get('/post-job',[JobPostController::class,'index']); //rotue of form of job posting.
