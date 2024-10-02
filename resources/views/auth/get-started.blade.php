6139241a45a151488ee57767b020447353a7e559
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-02 18:03:59 +0545

    link-css

--- resources/views/auth/get-started.blade.php
+++ resources/views/auth/get-started.blade.php
@@ -10,6 +10,7 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
         rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/components/get-started.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
