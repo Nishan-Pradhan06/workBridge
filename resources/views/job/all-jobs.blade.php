9108ec4c0bbfd2c11bc73dff9651826afe4281d1
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-21 21:08:50 +0545

    fix-post-issue

--- resources/views/job/all-jobs.blade.php
+++ resources/views/job/all-jobs.blade.php
@@ -9,10 +9,10 @@
                 <h5>{{$jobPosts->title}}</h5>
             </div>
             <div class="card-body">
-                <p class="card-text">{!! str_replace(['{', '}'], '', $jobPosts->des) !!}</p>
-                <p class="card-text">Duration: {{$jobPosts->duration}}</p>
+                <p class="card-text">{{$jobPosts->description}}</p>
+                <p class="card-text">Duration: {{$jobPosts->deadline}}</p>
                 <p class="card-text">Budget: Rs. {{$jobPosts->budget}}</p>
-                <p class="card-text">Tech Stack: {{$jobPosts->skill}}</p>
+                <p class="card-text">Tech Stack: {{$jobPosts->skills}}</p>
                 <a href="{{ url('/edit/' . $jobPosts->id) }}" class="btn btn-primary">edit</a>
                 <a href="/proposal" class="btn btn-primary">delete</a>
             </div>
