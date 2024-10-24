4d144e32f85a7140293ae9c0a1f8278de02cf9b0
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-24 20:16:40 +0545

    update-component

--- resources/views/features/job/all-jobs.blade.php
+++ resources/views/features/job/all-jobs.blade.php
@@ -1,44 +1,62 @@
+@extends('components/clients/client_nav')
+@section('content')
+<style>
+    .custom-card {
+        cursor: pointer;
+    }
+
+    .custom-card:hover h5 a {
+        text-decoration: underline;
+        color: #3a445a;
+    }
+
+    .card-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .left a {
+        color: black;
+    }
+
+    .mid {
+        text-align: center;
+    }
+</style>
 <div class="container">
     <div class="px-5">
         <br>
-        @if(request()->is('all-jobs'))
         <h3>All Jobs</h3>
-        @else
-        <h3>Discover Jobs</h3>
-        @endif
+
         @foreach($jobPost as $jobPosts)
-        <div class="card mb-4">
+        <div class="card mb-4 custom-card">
             <div class="card-header">
-                <p style="font-size: 10px;">Posted {{ \Carbon\Carbon::parse($jobPosts->created_at)->diffForHumans() }}</p>
-                <h5>{{$jobPosts->title}}</h5>
-            </div>
-            <div class="card-body">
-                <p class="card-text">{{$jobPosts->description}}</p>
-                <p class="card-text">Duration: {{$jobPosts->deadline}}</p>
-                <p class="card-text">Budget: Rs. {{$jobPosts->budget}}</p>
-                <p class="card-text">Tech Stack: {{$jobPosts->skills}}</p>
+                <div class="left">
+                    <h5><a href="http://">{{$jobPosts->title}}</a></h5>
+                    <p style="font-size: 12px;">Created {{ \Carbon\Carbon::parse($jobPosts->created_at)->diffForHumans() }} by You</p>
+                </div>
+                <div class="mid">
+                    <p style="font-size: 24px;">0</p>
+                    <h6>Proposals</h6>
 
-                <!-- Logic to differentiate between routes -->
+                </div>
+                <div class="right">
+                    @if($jobPosts->deleted_at)
+                    <!-- Soft deleted, show restore and permanently delete options -->
+                    <a href="{{ url('/restore/' . $jobPosts->id) }}" class="btn btn-success">Reuse Posting</a>
+                    <a href="{{ url('/delete/' . $jobPosts->id) }}" class="btn btn-danger">Delete</a>
+                    @else
+                    <!-- Active job, show edit, soft delete, and delete options -->
+                    <a href="{{ url('/edit/' . $jobPosts->id) }}" class="btn btn-primary">Edit</a>
+                    <a href="{{ url('/remove/' . $jobPosts->id) }}" class="btn btn-warning">Remove Posting</a>
+                    <a href="{{ url('/delete/' . $jobPosts->id) }}" class="btn btn-danger">Delete</a>
+                    @endif
+                </div>
 
-                @if(request()->is('all-jobs')) <!--  yadi all-jobs vhane route hit hunxa vhane yo buttons khulxa-->
-                @if($jobPosts->deleted_at)
-                <!-- Soft deleted, show restore and permanently delete options -->
-                <a href="{{ url('/restore/' . $jobPosts->id) }}" class="btn btn-success">Restore Posting</a>
-                <a href="{{ url('/delete/' . $jobPosts->id) }}" class="btn btn-danger">Delete</a>
-                @else
-                <!-- Active job, show edit, soft delete, and delete options -->
-                <a href="{{ url('/edit/' . $jobPosts->id) }}" class="btn btn-primary">Edit</a>
-                <a href="{{ url('/remove/' . $jobPosts->id) }}" class="btn btn-warning">Remove Posting</a>
-                <a href="{{ url('/delete/' . $jobPosts->id) }}" class="btn btn-danger">Delete</a>
-                @endif
-                @elseif(request()->is('find-job')) <!--  yadi find-jobs vhane route hit hunxa vhane yo buttons khulxa-->
-                <!-- For "find-jobs", only show the Apply button for non-deleted jobs -->
-                @if(!$jobPosts->deleted_at)
-                <a href="{{ url('/apply/' . $jobPosts->id) }}" class="btn btn-success">Apply</a>
-                @endif
-                @endif
             </div>
         </div>
         @endforeach
     </div>
 </div>
+@endsection
--- resources/views/users/clients/clients.blade.php
+++ resources/views/users/clients/clients.blade.php
@@ -1,4 +1,4 @@
-@extends('components.navBar') <!--IMPORTING THE FILES FROM COMPOENTS/NAVBAR.BLADE.PHP-->
+@extends('components.clients.client_nav') <!--IMPORTING THE FILES FROM COMPOENTS/NAVBAR.BLADE.PHP-->
 @section('title','Dashboard') <!--SETTINGUP THE TITLE-->
 @section('content') <!--START THE CONTENT FROM HERE-->
 <a href="/job-post" class="btn btn-primary">Post a Job</a>
