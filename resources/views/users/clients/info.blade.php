7be78f6cf2a6fb3d09f55b65e50e6ee63753c78e
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-31 22:55:11 +0545

    import components

--- resources/views/users/clients/info.blade.php
+++ resources/views/users/clients/info.blade.php
@@ -1,1 +1,6 @@
-<h1>client info</h1>
+@extends('components.clients.client_nav') <!--IMPORTING THE FILES FROM COMPOENTS/NAVBAR.BLADE.PHP-->
+@section('title','Dashboard') <!--SETTINGUP THE TITLE-->
+@section('content') <!--START THE CONTENT FROM HERE-->
+
+<h1>Setting</h1>
+@endsection <!--END THE CONTENT FROM HERE-->
