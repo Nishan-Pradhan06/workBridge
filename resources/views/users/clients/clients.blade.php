b8196cdf0c1eda77b0183732ae375787027bf652
nabin1200 <nabinbasnet1200@gmail.com>
2024-11-27 15:55:11 +0545

    created client dashboard

--- resources/views/users/clients/clients.blade.php
+++ resources/views/users/clients/clients.blade.php
@@ -2,7 +2,66 @@
 @section('title','Dashboard') <!--SETTINGUP THE TITLE-->
 @section('content') <!--START THE CONTENT FROM HERE-->
 
+<style>
 
+.container {
+    padding: 20px;
+    height:50vh;
+
+    
+}
+h1{
+    margin:0 200px;
+}
+
+.section {
+    text
+    background: white;
+    margin: 10px 0;
+    height:35vh;
+    padding: 15px;
+    border-radius: 5px;
+    box-shadow:  2px 5px rgba(0, 0, 0, 0.1);
+    text-align:cenetr;
+}
+h2{
+    text-align:center;
+}
+p,button{
+    text-align:center;
+    font-size:20px;
+}
+
+button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    text-align:center;
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+
+</style>
+<h1>Welcome to Your Dashboard</h1>
+
+
+       
+
+    <div class="container">
+    
+        <div id="intro" class="section">
+            <h2>Getting Started,</h2><br>
+            <p>Welcome to our freelancing platform!<br> 
+            Here you can post any projects job and also you can find job.<br>
+             Let's get you started posting Jobs!</p>
+            <button onclick="postProject()">Post Project</button>
+        </div>
+    </div>
 
 
 
