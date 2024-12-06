9c1f06b47c2fa47c360c0bcd3ba54948e3888258
nabin1200 <nabinbasnet1200@gmail.com>
2024-12-06 07:59:00 +0545

    contractModel

--- app/Http/Controllers/ContractModelController.php
+++ app/Http/Controllers/ContractModelController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\ContractModel;
+use App\Models\JobPost;
+use App\Models\JobProposal;
+use App\Models\Profile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ContractModelController extends Controller
 {
@@ -12,8 +16,29 @@ class ContractModelController extends Controller
      */
     public function showContractPage()
     {
-        return view('features.contracts.contract');
+        $userId = Auth::user()->id;
+        $jobProposals = JobProposal::all();
+        // $jobPosts=JobPost::where('user_id', $userId)->get();
+        $jobPosts = JobPost::whereIn('id', $jobProposals->pluck('job_id'))->get();
+
+        return view('features.contracts.contract',compact('jobProposals', 'jobPosts'));
     }
+
+    // public function showContractPage($proposalId)
+    // {
+    //     // Retrieve the proposal by its ID
+    //     $proposal = JobProposal::find($proposalId);
+        
+    //     if (!$proposal) {
+    //         return redirect()->back()->with('error', 'Proposal not found');
+    //     }
+
+    //     // Retrieve freelancer profile data
+    //     $freelancerProfile = Profile::where('user_id', $proposal->user_id)->first();
+
+    //     // Pass the proposal and freelancer profile to the view
+    //     return view('features.contracts.contract', compact('proposal', 'freelancerProfile'));
+    // }
 
     /**
      * Show the form for creating a new resource.
@@ -36,7 +61,7 @@ class ContractModelController extends Controller
      */
     public function show(ContractModel $contractModel)
     {
-        //
+        //z
     }
 
     /**
