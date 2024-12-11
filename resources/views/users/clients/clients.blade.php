d768be79af5086b27716d2815e7659926a896e03
nishanpradhan <nishanpradhan500@gmail.com>
2024-12-11 22:21:49 +0545

    client dashboard

--- resources/views/users/clients/clients.blade.php
+++ resources/views/users/clients/clients.blade.php
@@ -3,61 +3,62 @@
 @section('content') <!--START THE CONTENT FROM HERE-->
 
 <style>
+    .container {
+        padding: 20px;
+        height: 50vh;
+    }
 
-.container {
-    padding: 20px;
-    height:50vh;   
-}
-.welcome-section{
-    display: flex;
-    justify-content:space-between;
-    align-items:center;
-}
+    .welcome-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-.section {
-    background: white;
-    margin: 10px 0;
-    height:35vh;
-    padding: 15px;
-    border-radius: 5px;
-    box-shadow:  2px 5px rgba(0, 0, 0, 0.1);
-    text-align:cenetr;
-}
-h2{
-    text-align:center;
-}
-.section>p,button{
-    text-align:center;
-    font-size:20px;
-}
+    .section {
+        background: white;
+        margin: 10px 0;
+        height: 35vh;
+        padding: 15px;
+        border-radius: 5px;
+        box-shadow: 2px 5px rgba(0, 0, 0, 0.1);
+        text-align: cenetr;
+    }
 
-button {
-    background-color: #007bff;
-    color: white;
-    border: none;
-    text-align:center;
-    padding: 10px 15px;
-    border-radius: 5px;
-    cursor: pointer;
-}
+    h2 {
+        text-align: center;
+    }
 
-button:hover {
-    background-color: #0056b3;
-}
+    .section>p,
+    button {
+        text-align: center;
+        font-size: 20px;
+    }
 
+    button {
+        background-color: #007bff;
+        color: white;
+        border: none;
+        text-align: center;
+        padding: 10px 15px;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
 </style>
 <div class="container">
     <div class="welcome-section">
-    <h3>Welcome, {{auth()->user()->name}}</h3>
-    <a href="/job-post" class="btn btn-primary">Post a Job</a>
+        <h3>Welcome, {{auth()->user()->name}}</h3>
+        <a href="/job-post" class="btn btn-primary">Post a Job</a>
     </div>
-        
-        <div id="intro" class="section">
-            <p> 
-            Here you can post any projects job and also you can find job.
-             Let's get you started posting Jobs!</p>
-            
-        </div>
+
+    <div id="intro" class="section">
+        <p>
+            new content</p>
+
     </div>
+</div>
 @include('components.footer')
 @endsection <!--END THE CONTENT FROM HERE-->
