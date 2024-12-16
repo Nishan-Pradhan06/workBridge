fbe211b6f739aafc599d7bac8d12f157175c8207
nabin1200 <nabinbasnet1200@gmail.com>
2024-12-16 21:47:12 +0545

    adding breadcrumb

--- resources/views/features/projects/tracking.blade.php
+++ resources/views/features/projects/tracking.blade.php
@@ -1,5 +1,12 @@
 @extends('components/clients/client_nav') <!--IMPORT THE COMPONENTS-->
 @section('content')
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb" style="padding-left: 5em;">
+        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+        <li class="breadcrumb-item"><a href="#">Projects</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Assign Progress</li>
+    </ol>
+</nav>
 
 @include('components.footer')
 @endsection
